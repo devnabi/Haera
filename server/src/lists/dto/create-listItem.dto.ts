b928c76b1d8eea77844a08120a06efb77f1fcbbd
devnabi <klkl0812@naver.com>
2024-05-09 22:35:02 +0900

    @IsNotEmpty() 추가

--- server/src/lists/dto/create-listItem.dto.ts
+++ server/src/lists/dto/create-listItem.dto.ts
@@ -1,6 +1,12 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateListItemDto {
     id: string | bigint;
+    
     list_id: string | bigint;
+
+    @IsNotEmpty()
     todo_text: string;
+
     status: boolean;
 }
