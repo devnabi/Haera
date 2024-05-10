20a2fe300e0425814e02a1b4941cac1f04a6c3e3
devnabi <klkl0812@naver.com>
2024-05-10 16:11:50 +0900

    엔티티 생성

---
+++ server/src/lists/list.entitiy.ts
@@ -0,0 +1,29 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { InquiryStatus } from "./list-inquiryStatus.enum";
+
+@Entity()
+export class List extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    user_id: number;
+
+    @Column()
+    inquiry_status: InquiryStatus;
+}
+
+@Entity()
+export class ListItem extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    list_id: number;
+
+    @Column()
+    todo_text: string;
+
+    @Column()
+    status: boolean;
+}
