8f96e06f512cfef43176286240d7c212079c45cd
devnabi <klkl0812@naver.com>
2024-05-11 00:29:37 +0900

    엔티티 연결

--- server/src/lists/lists.module.ts
+++ server/src/lists/lists.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ListsController } from './lists.controller';
 import { ListsService } from './lists.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { List, ListItem } from './list.entitiy';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([List, ListItem])
+  ],
   controllers: [ListsController],
   providers: [ListsService]
 })
