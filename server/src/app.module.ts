f5c55682afb42a3a3e5324e94f21800706c6995e
devnabi <klkl0812@naver.com>
2024-05-12 22:48:30 +0900

    auth 모듈 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ListsModule } from './lists/lists.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig),
-    ListsModule],
+    ListsModule,
+    AuthModule],
 })
 export class AppModule {}
