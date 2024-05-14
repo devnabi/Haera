0965b3dc9d7ebe940e9a5fbdf00b975f2757c322
devnabi <klkl0812@naver.com>
2024-05-14 16:00:02 +0900

    설치한 JWT와 Passport 모듈을 import 해주기

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret:'secretToken',
+      signOptions: {
+        expiresIn: 60 * 60
+      }
+    }),
     TypeOrmModule.forFeature([User])
   ],
   controllers: [AuthController],
