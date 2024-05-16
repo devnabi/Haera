c75c5e0b6ffffa89eb15f5322b9fe1b8c2719143
devnabi <klkl0812@naver.com>
2024-05-16 11:10:53 +0900

    listsModule에서도 AuthModule을 사용할 수 있게 적용

--- server/src/lists/lists.module.ts
+++ server/src/lists/lists.module.ts
@@ -3,10 +3,12 @@ import { ListsController } from './lists.controller';
 import { ListsService } from './lists.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { List, ListItem } from './list.entitiy';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([List, ListItem])
+    TypeOrmModule.forFeature([List, ListItem]),
+    AuthModule
   ],
   controllers: [ListsController],
   providers: [ListsService]
