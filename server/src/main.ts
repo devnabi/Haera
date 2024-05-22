f3524f2f7925537be7a0150a8d61c9c78c6f3907
devnabi <klkl0812@naver.com>
2024-05-22 14:25:05 +0900

    Server CORS 설정 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://127.0.0.1:8080', // 클라이언트의 주소
+    methods: 'GET,POST,PATCH',
+    allowedHeaders: 'Content-Type, Authorization'
+  });
   await app.listen(3000);
 }
 bootstrap();
