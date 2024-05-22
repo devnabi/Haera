ecf739caa43329e282f9a6cf1fad756ea52775d1
devnabi <klkl0812@naver.com>
2024-05-22 14:23:00 +0900

    Vue Proxy 설정

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,3 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  // 개발 서버 설정
+  devServer: {
+    // 프록시 설정
+    proxy: {
+      // 프록시 요청을 보낼 api의 시작 부분
+      '/auth': {
+        // 프록시 요청을 보낼 서버의 주소
+        target: 'http://127.0.0.1:3000'
+      }
+    }
+  }
+}
