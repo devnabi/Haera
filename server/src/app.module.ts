c93bc5e392a28419151bbbbfb95865c5878ac85c
devnabi <klkl0812@naver.com>
2024-05-22 14:18:48 +0900

    ConfigModule 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,15 @@ import { ListsModule } from './lists/lists.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
+    ConfigModule.forRoot({
+      envFilePath: ".env.configuration",
+      isGlobal: true,
+    }),
     ListsModule,
     AuthModule
   ],
