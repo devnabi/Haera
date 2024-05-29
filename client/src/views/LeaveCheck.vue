ad5bb273971ee079dc8b3d65255b56a325995047
devnabi <klkl0812@naver.com>
2024-05-29 16:37:03 +0900

    사용자 탈퇴 요청 추가

--- client/src/views/LeaveCheck.vue
+++ client/src/views/LeaveCheck.vue
@@ -20,15 +20,15 @@
                             <div class="col-6">
                                 <label for="exampleInputPassword1"
                                     class="col-form-label col-form-label-lg mt-4">Password</label>
-                                <input type="password" class="form-control form-control-lg" id="exampleInputPassword1"
-                                    placeholder="Password" autocomplete="off">
+                                <input type="password" v-model="password" class="form-control form-control-lg"
+                                    id="exampleInputPassword1" placeholder="Password" autocomplete="off">
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <button @click="confirmLeave" type="button" class="btn btn-secondary btn-lg my-5 mt-5">Leave</button>
-                <button type="button" class="btn btn-outline-primary btn-lg mx-2 my-5 mt-5">Cancel</button>
+                <button type="button" @click="confirmLeave" class="btn btn-secondary btn-lg my-5 mt-5">Leave</button>
+                <button type="button" @click="cancel" class="btn btn-outline-primary btn-lg mx-2 my-5 mt-5">Cancel</button>
             </fieldset>
         </form>
 
@@ -36,13 +36,53 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'LeaveCheck',
+
+    data() {
+        return {
+            password: "",
+            token: ""
+        }
+    },
+
+    mounted() {
+        this.token = localStorage.getItem("accessToken");
+    },
+
     methods: {
-        // 비밀번호가 틀렸거나, 입력하지 않은 채로 버튼을 누르면 비밀번호를 확인하라는 빨간 문구 띄어주기
         confirmLeave() {
-            if (confirm("Are you sure you want to leave 'Haera'?")) {
-                this.$router.push('/leavesuccess');
+            const confirmation = confirm("Are you sure you want to leave 'Haera'?")
+            if (confirmation && this.password) {
+                this.leaveRequest();
+            } else {
+                this.$router.push("/leavecheck");
+            }
+        },
+
+        cancel() {
+            this.$router.push("/");
+        },
+
+        // 비밀번호가 틀렸거나, 입력하지 않은 채로 버튼을 누르면 비밀번호를 확인하라는 빨간 문구 띄어주기
+        async leaveRequest() {
+            try {
+                const getResponse = await axios.get(`/auth/getValidateUser/${this.token}`);
+                const userData = getResponse.data;
+                const deleteResponse = await axios.delete(`/auth/delete/${userData.id}`, {
+                    data: {
+                        password: this.password
+                    }
+                });
+                console.log("deleteResponse: ", deleteResponse);
+                localStorage.removeItem("accessToken");
+                localStorage.removeItem("isSignedIn");
+                this.$router.push("/leavesuccess");
+
+            } catch (error) {
+                console.log("error", error);
             }
         }
     }
