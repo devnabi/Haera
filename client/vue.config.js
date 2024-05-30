b45ddc3f98a8e4197e89aece8cbc909051e74a91
devnabi <klkl0812@naver.com>
2024-05-30 20:52:07 +0900

    Vue Proxy 설정 추가

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,15 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  // 개발 서버 설정
   devServer: {
-    // 프록시 설정
     proxy: {
-      // 프록시 요청을 보낼 api의 시작 부분
-      '/auth': {
-        // 프록시 요청을 보낼 서버의 주소
-        target: 'http://127.0.0.1:3000'
+      "/auth": {
+        target: "http://127.0.0.1:3000",
+      },
+      "/lists": {
+        target: "http://127.0.0.1:3000",
       }
     }
   }
-})
+});
