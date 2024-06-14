aa21f28339ade5a72d7ec3a532e661ba931fb4b9
devnabi <klkl0812@naver.com>
2024-06-14 15:35:54 +0900

    페이지네이션을 위한 bootstrap-vue3 설치 & 적용

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index.js';
+import { BootstrapVue3 } from 'bootstrap-vue-3';
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootswatch/dist/minty/bootstrap.min.css";
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(BootstrapVue3);
+app.mount('#app');
