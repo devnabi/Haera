c9b91e3a6957208fd7bbf50ee612d515cc36dd49
devnabi <klkl0812@naver.com>
2024-06-19 21:55:21 +0900

    env파일 추가 & Vue Proxy 수정

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,10 +4,10 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       "/auth": {
-        target: "http://127.0.0.1:3000",
+        target: process.env.SERVER_URL ?? "http://localhost:3000",
       },
       "/lists": {
-        target: "http://127.0.0.1:3000",
+        target: process.env.SERVER_URL ?? "http://localhost:3000",
       }
     }
   }
